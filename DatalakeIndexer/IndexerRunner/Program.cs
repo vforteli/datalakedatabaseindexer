62e47426726bc29d3efc65aa0880006f4dae7ccb
Verner Fortelius <verner@fortelius.net>
2024-12-18 16:33:29 +0200

    test metadata upsert

--- DatalakeIndexer/IndexerRunner/Program.cs
+++ DatalakeIndexer/IndexerRunner/Program.cs
@@ -1,8 +1,10 @@
-﻿using DatabaseIndexer;
+﻿using System.Text.Json;
+using DatabaseIndexer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Npgsql;
 using PGDatabaseIndexer;
+using PathMetadataRowType = PGDatabaseIndexer.PathMetadataRowType;
 
 var config = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true).Build();
 var loggerFactory = LoggerFactory.Create(o => o.AddSimpleConsole(c => c.SingleLine = true));
@@ -22,16 +24,39 @@ using var dataSource =
 var pgServerIndexer = new PGServerIndexer(dataSource, loggerFactory.CreateLogger<PGServerIndexer>());
 
 logger.LogInformation("Starting path upsert...");
-var rowsAffected = pgServerIndexer.UpsertPathsAsync(mockPaths.Select(o => new PGDatabaseIndexer.PathRowType
+
+var mockMetadata = new
 {
-    created_on = o.CreatedOn,
-    deleted_on = o.DeletedOn,
-    etag = o.ETag,
-    filesystem_name = o.FilesystemName,
-    last_modified = o.LastModified,
-    path = o.Path,
-})).ToBlockingEnumerable();
-logger.LogInformation("Upsert done, rows affected {rows}", rowsAffected.Count());
+    someproperty = "somevalue",
+    anotherproperty = "anothervalue",
+    hurr = "durr",
+    foooooo = "some slightly longer property goes here",
+};
+
+var mockMetadataJson = JsonSerializer.Serialize(mockMetadata);
+
+foreach (var mockPathChunk in mockPaths.Chunk(100000))
+{
+    var upsertedPaths = pgServerIndexer.UpsertPathsAsync(mockPathChunk.Select(o => new PGDatabaseIndexer.PathRowType
+    {
+        created_on = o.CreatedOn,
+        deleted_on = o.DeletedOn,
+        etag = o.ETag,
+        filesystem_name = o.FilesystemName,
+        last_modified = o.LastModified,
+        path = o.Path,
+    })).ToBlockingEnumerable();
+
+    await pgServerIndexer.UpsertPathsMetadataAsync(upsertedPaths.Select(o =>
+        new PathMetadataRowType
+        {
+            etag = "notrelevant",
+            path_key = o.path_key,
+            metadata_json = mockMetadataJson,
+        }));
+}
+
+logger.LogInformation("Upsert done");
 
 //
 //
